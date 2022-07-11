76316d6e013e243940f05e5bc4f192357bea50f8
Umesha  PM <umeshpm4@gmail.com>
2022-07-11 19:09:53 +0530

    JS-HANDSON-6

---
+++ 5.Substring_Check.js
@@ -0,0 +1,13 @@
+//Q-5. Substring Check
+
+//You are given two strings S1 and S2, you need to check whether the string S1 is a substring of string S2 or not.
+
+var Substring_Check = (S1, S2) => 
+{
+    if(S1.includes(S2)){
+      return ("YES");
+    }
+    return ("NO");
+};
+ 
+console.log(Substring_Check("Hii this is Prepbuddy","Prepbuddy"));
